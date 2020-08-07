9cf8748cadf8d354b9178108ef30265c97f65a9e
PioKl <kosiu342@gmail.com>
2020-08-07 20:33:23 +0200

    App.js|App is now in components, add import of Hero

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.scss';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <div className="App container">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
