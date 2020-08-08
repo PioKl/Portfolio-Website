4c15dd01a0c8fea2ec47db5ea9f45288279e9d63
PioKl <kosiu342@gmail.com>
2020-08-08 18:48:44 +0200

    App.js|Add Projects component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../style/App.scss';
 import Hero from './Hero';
+import Projects from './Projects';
 
 function App() {
   return (
     <div className="App container">
       <Hero />
+      <Projects />
     </div>
   );
 }
