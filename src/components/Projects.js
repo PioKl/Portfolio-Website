b208be27574aa9d8f31f2c6a3b5c477bbcb76799
PioKl <kosiu342@gmail.com>
2020-08-08 18:49:56 +0200

    Projects.js|New component Projects

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Projects.scss';
+
+const Projects = () => {
+    return (
+        <div id="projects" className="projects">
+
+        </div>
+    );
+}
+
+export default Projects;
