5ab8993473e936a8f74dac27ce91badf6e459376
PioKl <kosiu342@gmail.com>
2020-08-10 19:55:45 +0200

    Hero.js|Add anchorlinks to id in navigation, scroll-navigation also in button, add Scrollspy with that method all handlers are now not needed, some classes are now in different place

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,67 +1,68 @@
 import React, { useState } from 'react';
+import Scrollspy from 'react-scrollspy';
 import '../style/Hero.scss';
 
 const Hero = () => {
-    const [mainSite, setMainSite] = useState(true);
-    const [projects, setProjects] = useState(false);
-    const [tools, setTools] = useState(false);
-    const [aboutMe, setAboutMe] = useState(false);
-    const [contact, setContact] = useState(false);
-
-    const handleScrollMainSite = () => {
-        setMainSite(true);
-        setProjects(false);
-        setTools(false);
-        setAboutMe(false);
-        setContact(false);
-    }
-    const handleScrollProjects = () => {
-        setMainSite(false);
-        setProjects(true);
-        setTools(false);
-        setAboutMe(false);
-        setContact(false);
-    }
-    const handleScrollTools = () => {
-        setMainSite(false);
-        setProjects(false);
-        setTools(true);
-        setAboutMe(false);
-        setContact(false);
-    }
-    const handleScrollAboutMe = () => {
-        setMainSite(false);
-        setProjects(false);
-        setTools(false);
-        setAboutMe(true);
-        setContact(false);
-    }
-    const handleScrollContact = () => {
-        setMainSite(false);
-        setProjects(false);
-        setTools(false);
-        setAboutMe(false);
-        setContact(true);
-    }
-
+    /*     const [mainSite, setMainSite] = useState(true);
+        const [projects, setProjects] = useState(false);
+        const [tools, setTools] = useState(false);
+        const [aboutMe, setAboutMe] = useState(false);
+        const [contact, setContact] = useState(false);
+    
+        const handleScrollMainSite = () => {
+            setMainSite(true);
+            setProjects(false);
+            setTools(false);
+            setAboutMe(false);
+            setContact(false);
+        }
+        const handleScrollProjects = () => {
+            setMainSite(false);
+            setProjects(true);
+            setTools(false);
+            setAboutMe(false);
+            setContact(false);
+        }
+        const handleScrollTools = () => {
+            setMainSite(false);
+            setProjects(false);
+            setTools(true);
+            setAboutMe(false);
+            setContact(false);
+        }
+        const handleScrollAboutMe = () => {
+            setMainSite(false);
+            setProjects(false);
+            setTools(false);
+            setAboutMe(true);
+            setContact(false);
+        }
+        const handleScrollContact = () => {
+            setMainSite(false);
+            setProjects(false);
+            setTools(false);
+            setAboutMe(false);
+            setContact(true);
+        }
+     */
     return (
         <div id="mainSite" className="hero-container">
             <div className="navigation">
                 <ul className="navigation__itemsList">
-                    <li onClick={handleScrollMainSite} className="navigation__item navigation__item">
+                    <li /* onClick={handleScrollMainSite} */ className="navigation__item navigation__item">
                         <a href="#mainSite">Strona <span className="navigation__item--mainSite">główna</span></a>
                     </li>
                     <ul className="navigation__itemsList--middle">
-                        <li onClick={handleScrollProjects} className="navigation__item navigation__item--middle">
+                        <li /* onClick={handleScrollProjects} */ className="navigation__item navigation__item--middle">
                             <a href="#projects">Projekty</a>
                         </li>
-                        <li onClick={handleScrollTools} className="navigation__item navigation__item--middle">
+                        <li /* onClick={handleScrollTools} */ className="navigation__item navigation__item--middle">
                             <a href="#tools">Narzędzia</a>
                         </li>
-                        <li onClick={handleScrollAboutMe} className="navigation__item navigation__item--middle">
+                        <li /* onClick={handleScrollAboutMe} */ className="navigation__item navigation__item--middle">
                             <a href="#aboutMe">O mnie</a>
                         </li>
-                        <li onClick={handleScrollContact} className="navigation__item navigation__item--middle">
+                        <li /* onClick={handleScrollContact} */ className="navigation__item navigation__item--middle">
                             <a href="#contact">Kontakt</a>
                         </li>
                     </ul>
@@ -69,17 +70,26 @@ const Hero = () => {
                 </ul>
             </div>
             <div className="scroll-navigation">
-                <ul className="scroll-navigation__itemsList">
-                    <a href="#mainSite"><li onClick={handleScrollMainSite} className={`scroll-navigation__item scroll-navigation__item--mainSite ${mainSite && 'scroll-navigation__item--active'}`}></li></a>
-                    <a href="#projects"><li onClick={handleScrollProjects} className={`scroll-navigation__item scroll-navigation__item--projects ${projects && 'scroll-navigation__item--active'}`}></li></a>
-                    <a href="#tools"><li onClick={handleScrollTools} className={`scroll-navigation__item scroll-navigation__item--tools ${tools && 'scroll-navigation__item--active'}`}></li></a>
-                    <a href="#aboutMe"><li onClick={handleScrollAboutMe} className={`scroll-navigation__item scroll-navigation__item--aboutMe ${aboutMe && 'scroll-navigation__item--active'}`}></li></a>
-                    <a href="#contact"><li onClick={handleScrollContact} className={`scroll-navigation__item scroll-navigation__item--contact ${contact && 'scroll-navigation__item--active'}`}></li></a>
-                </ul>
+                <Scrollspy className="scroll-navigation__itemsList" items={['mainSite', 'projects', 'tools', 'aboutMe', 'contact']} currentClassName="scroll-navigation__item--active" >
+                    <a href="#mainSite" /* onClick={handleScrollMainSite} */ className={`scroll-navigation__item scroll-navigation__item--mainSite`}>mainSite</a>
+                    <a href="#projects" /* onClick={handleScrollProjects} */ className={`scroll-navigation__item scroll-navigation__item--projects`}>projects</a>
+                    <a href="#tools" /* onClick={handleScrollTools} */ className={`scroll-navigation__item scroll-navigation__item--tools`}>tools</a>
+                    <a href="#aboutMe" /* onClick={handleScrollAboutMe} */ className={`scroll-navigation__item scroll-navigation__item--aboutMe`}>aboutMe</a>
+                    <a href="#contact" /* onClick={handleScrollContact} */ className={`scroll-navigation__item scroll-navigation__item--contact`}>contact</a>
+
+
+
+                    {/*                         <a href="#mainSite"><li onClick={handleScrollMainSite} className={`scroll-navigation__item scroll-navigation__item--mainSite ${mainSite && 'scroll-navigation__item--active'}`}></li></a>
+                        <a href="#projects"><li onClick={handleScrollProjects} className={`scroll-navigation__item scroll-navigation__item--projects ${projects && 'scroll-navigation__item--active'}`}></li></a>
+                        <a href="#tools"><li onClick={handleScrollTools} className={`scroll-navigation__item scroll-navigation__item--tools ${tools && 'scroll-navigation__item--active'}`}></li></a>
+                        <a href="#aboutMe"><li onClick={handleScrollAboutMe} className={`scroll-navigation__item scroll-navigation__item--aboutMe ${aboutMe && 'scroll-navigation__item--active'}`}></li></a>
+                        <a href="#contact"><li onClick={handleScrollContact} className={`scroll-navigation__item scroll-navigation__item--contact ${contact && 'scroll-navigation__item--active'}`}></li></a> */}
+                </Scrollspy>
             </div>
+
             <div className="hero-info">
                 <h1 className="hero-info__heading">Cześć, jestem Piotr i tworzę aplikacje w React oraz strony internetowe!</h1>
-                <a href="#projects"><button onClick={handleScrollProjects} className="hero-info__button button">Sprawdź moje projekty</button></a>
+                <a href="#projects"><button /* onClick={handleScrollProjects} */ className="hero-info__button button">Sprawdź moje projekty</button></a>
             </div>
 
 
