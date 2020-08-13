a70666947180d3d1ffc30f39cec43a775bbf439e
PioKl <kosiu342@gmail.com>
2020-08-13 19:32:35 +0200

    App.js|Add event to update window.innerWidth to create Mobile version, add new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style/App.scss';
 import Hero from './Hero';
 import Projects from './Projects';
+import Tools from './Tools';
+import AboutMe from './AboutMe';
+import Contact from './Contact';
 
 function App() {
+
+  //Sprawdzanie innerWidth okna przeglądarki i przypisanie wartości do zmiennej
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const updateWindowInnerWith = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", updateWindowInnerWith);
+
+    return () => window.removeEventListener("resize", updateWindowInnerWith)
+
+  }, []);
+
+  const isMobile = width <= 726;
+
   return (
-    <div className="App container">
-      <Hero />
-      <Projects />
-    </div>
+
+    isMobile ?
+
+      <div>
+        <h1>Test</h1>
+      </div>
+
+      :
+
+      <div className="App container">
+        <Hero />
+        <Projects />
+        <Tools />
+        <AboutMe />
+        <Contact />
+      </div>
+
   );
 }
 
