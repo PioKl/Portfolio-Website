1eaabe7b1fcc128a7c55276133bfc14a2e6c1a5d
PioKl <kosiu342@gmail.com>
2020-08-13 19:34:43 +0200

    AboutMe.js|Add new functional component AboutMe

---
+++ src/components/AboutMe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../style/AboutMe.scss";
+
+const AboutMe = () => {
+    return (
+        <div id="aboutMe" className="aboutMe-container"></div>
+    );
+}
+
+export default AboutMe;
