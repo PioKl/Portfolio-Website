c441cd14124e5e1e68ab5db8e53be90f5d23cd89
PioKl <kosiu342@gmail.com>
2020-08-13 19:35:06 +0200

    Contact.js|Add new functional component Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../style/Contact.scss";
+
+const Contact = () => {
+    return (
+        <div id="contact" className="contact-container"></div>
+    );
+}
+
+export default Contact;
