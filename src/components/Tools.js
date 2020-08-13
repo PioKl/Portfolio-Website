3d07d7be94f397b9628a91d2f79b8ca137337bdd
PioKl <kosiu342@gmail.com>
2020-08-13 19:33:37 +0200

    Tools.js|Add new functional component Tools

---
+++ src/components/Tools.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../style/Tools.scss";
+
+const Tools = () => {
+    return (
+        <div id="tools" className="tools-container"></div>
+    );
+}
+
+export default Tools;
