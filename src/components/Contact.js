3cc8f1965316078b9388d8e00cb3c09a980d6c47
PioKl <kosiu342@gmail.com>
2020-08-17 21:26:05 +0200

    Contact.js|Add new classes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,19 @@ import "../style/Contact.scss";
 
 const Contact = () => {
     return (
-        <div id="contact" className="contact-container"></div>
+        <div id="contact" className="contact-container">
+            <div className="contact-titleContainer">
+                <h1 className="contact-titleContainer__title">Wyślij mi wiadomość</h1>
+            </div>
+
+            <form className="contact-form" action="">
+                <input className="contact-form__input contact-form__input--name" type="text" placeholder="Imię" />
+                <input className="contact-form__input contact-form__input--email" type="email" placeholder="E-mail" />
+                <input className="contact-form__input contact-form__input--title" type="text" placeholder="Tytuł" />
+                <textarea className="contact-form__input contact-form__input--message" name="" id="" placeholder="Wiadomość"/* cols="30" rows="10" */></textarea>
+                <button className="button button--submit" type="submit">Wyślij</button>
+            </form>
+        </div>
     );
 }
 
