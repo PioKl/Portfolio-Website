4789dc429d8aca83889d6ebb21af5ac7e9cec27b
PioKl <kosiu342@gmail.com>
2020-08-18 21:29:49 +0200

    Contact.js|Add aos fades and removal of one input with name (testing)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,16 @@ import "../style/Contact.scss";
 const Contact = () => {
     return (
         <div id="contact" className="contact-container">
-            <div className="contact-titleContainer">
+            <div data-aos="flip-left" className="contact-titleContainer">
                 <h1 className="contact-titleContainer__title">Wyślij mi wiadomość</h1>
             </div>
 
             <form className="contact-form" action="">
-                <input className="contact-form__input contact-form__input--name" type="text" placeholder="Imię" />
-                <input className="contact-form__input contact-form__input--email" type="email" placeholder="E-mail" />
-                <input className="contact-form__input contact-form__input--title" type="text" placeholder="Tytuł" />
-                <textarea className="contact-form__input contact-form__input--message" name="" id="" placeholder="Wiadomość"/* cols="30" rows="10" */></textarea>
-                <button className="button button--submit" type="submit">Wyślij</button>
+                {/* <input className="contact-form__input contact-form__input--name" type="text" placeholder="Imię" /> */}
+                <input data-aos="fade-right" className="contact-form__input contact-form__input--email" type="email" placeholder="E-mail" />
+                <input data-aos="fade-left" className="contact-form__input contact-form__input--title" type="text" placeholder="Tytuł" />
+                <textarea data-aos="fade-right" className="contact-form__input contact-form__input--message" name="" id="" placeholder="Wiadomość"/* cols="30" rows="10" */></textarea>
+                <button data-aos="fade-left" className="button button--submit" type="submit">Wyślij</button>
             </form>
         </div>
     );
