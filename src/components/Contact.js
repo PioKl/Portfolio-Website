8fb485ce6d4473b9c20bb42da7da5b800f8bf772
PioKl <kosiu342@gmail.com>
2020-09-17 15:01:28 +0200

    Contact.js|Unnecessary comments removal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -38,10 +38,9 @@ const Contact = () => {
             </div>
 
             <form className="contact-form" action="" onSubmit={handleSendMessage}>
-                {/* <input className="contact-form__input contact-form__input--name" type="text" placeholder="Imię" /> */}
                 <input data-aos="fade-right" className="contact-form__input contact-form__input--email" type="email" name="email" placeholder="E-mail" required />
                 <input data-aos="fade-left" className="contact-form__input contact-form__input--title" type="text" placeholder="Tytuł" name="title" required />
-                <textarea data-aos="fade-right" className="contact-form__input contact-form__input--message" name="message" id="" placeholder="Wiadomość" required/* cols="30" rows="10" */></textarea>
+                <textarea data-aos="fade-right" className="contact-form__input contact-form__input--message" name="message" id="" placeholder="Wiadomość" required></textarea>
                 <button data-aos="fade-left" className="button button--submit" type="submit">Wyślij</button>
             </form>
             {notification &&
